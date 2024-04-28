04f065f462113d88ec59163c0ccaa7297b03af3e
VishakhaSenthilNathan <vishakhasenthilnathan@gmail.com>
2024-04-28 23:50:19 +0530

    Vishakha | Basic React calculator

--- src/App.js
+++ src/App.js
@@ -6,44 +6,41 @@ function App() {
     const [input, setInput] = useState("");
     const [result, setResult] = useState();
     const buttonStyle = {
-        padding: "15px",
-        margin: "4px"
+        padding: "15px", margin: "4px"
     }
 
     const handleCalculation = () => {
+        if (input === "") {
+            setResult("Error");
+            return;
+        }
         const currentInput = input;
         const numbers = currentInput.split(/[\+\-\*\/]/);
         const operators = currentInput.split(/\d/).filter((item) => item !== "");
         for (let i = 0; i < operators.length; i++) {
-            console.log(numbers, operators);
-            console.log("i",i);
+
             if (operators[i] === "*") {
                 numbers[i] = parseInt(numbers[i]) * parseInt(numbers[i + 1]);
                 numbers.splice(i + 1, 1);
                 operators.splice(i, 1);
                 i--;
-                console.log("i*",i);
 
             } else if (operators[i] === "/") {
                 numbers[i] = parseInt(numbers[i]) / parseInt(numbers[i + 1]);
                 numbers.splice(i + 1, 1);
                 operators.splice(i, 1);
                 i--;
-                console.log("i/",i);
 
             } else if (operators[i] === "+") {
                 numbers[i] = parseInt(numbers[i]) + parseInt(numbers[i + 1]);
                 numbers.splice(i + 1, 1);
                 operators.splice(i, 1);
                 i--;
-                console.log("i+",i);
-
             } else if (operators[i] === "-") {
                 numbers[i] = parseInt(numbers[i]) - parseInt(numbers[i + 1]);
                 numbers.splice(i + 1, 1);
                 operators.splice(i, 1);
                 i--;
-                console.log("i-",i);
             }
         }
         if (operators.length !== 0) {
@@ -52,22 +49,21 @@ function App() {
             setResult(numbers[0]);
         }
     }
-    return (
-        <div className="App">
+    return (<div className="App">
             <h1>React Calculator</h1>
             <input type="text" style={{margin: "10px"}} name="input" value={input}></input>
             <div>{result}</div>
             <div style={{display: "flex", flexWrap: "wrap", padding: "10px", justifyContent: "center", gap: "1"}}>
                 <div style={{width: "200px", height: "200px"}}>
-                    <input type="button" value={7} style={buttonStyle} onClick={(e) => {
+                    <input type="button" value="7" style={buttonStyle} onClick={(e) => {
                         setInput((prevState) => prevState.concat("7"))
                     }}>
                     </input>
-                    <input type="button" value={8} style={buttonStyle} onClick={(e) => {
+                    <input type="button" value="8" style={buttonStyle} onClick={(e) => {
                         setInput((prevState) => prevState.concat("8"))
                     }}>
                     </input>
-                    <input type="button" value={9} style={buttonStyle} onClick={(e) => {
+                    <input type="button" value="9" style={buttonStyle} onClick={(e) => {
                         setInput((prevState) => prevState.concat("9"))
                     }}>
                     </input>
@@ -122,8 +118,7 @@ function App() {
                     </input>
                 </div>
             </div>
-        </div>
-    );
+        </div>);
 }
 
 export default App;
